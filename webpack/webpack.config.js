192b0498ff6f11cbeac959cbc561bd2d8275c304
IgorAlexeew <goscha.alekseew2015@yandex.ru>
2022-02-27 13:44:32 +0400

    Webpack dev server and HMR feature

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,26 +2,31 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    devServer: {
+      static: './dist',
+      hot: true,
     },
     plugins: [
       new HtmlWebpackPlugin({
-        title: 'Output Management',
+        title: 'Development',
       }),
     ],
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
         },
       ],
     },
